6f77ff68face15196f9ac13345bbbe3f4bd1e55a
MuhamadSaeed <mohamedsaiedz2156287@gmail.com>
2025-03-27 21:51:13 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+const quotes = [
+    {
+        qoute: "Be the change that you wish to see in the world",
+        by: "Mahatma Gandhi"
+    },
+    {
+        qoute: "A room without books is like a body without a soul",
+        by: "Marcus Tullius Cicero"
+    },
+    {
+        qoute: "So many books, so little time",
+        by: "Frank Zappa"
+    }
+];
+
+
+let btn = document.querySelector(".btnQoute");
+let qoutePara = document.querySelector(".qoute-cont q");
+let writer = document.querySelector(".qoute-writer");
+let lastItem = -1;
+
+btn.addEventListener("click", () => {
+
+    let randomNumber = Math.floor(Math.random() * quotes.length);
+
+    console.log(randomNumber);
+    
+    while (randomNumber === lastItem) {
+        randomNumber = Math.floor(Math.random() * quotes.length);
+        console.log("");
+    }
+
+
+    lastItem = randomNumber;
+
+    let newQoute = quotes[randomNumber].qoute;
+    qoutePara.textContent = newQoute;
+
+    let newWriter = quotes[randomNumber].by;
+    writer.textContent = newWriter;
+});
